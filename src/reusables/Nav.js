dab9ee848b5b2e28af6d36f313b0c05c0c9dd6b1
Sayak Mukhopadhyay <msayak1269@gmail.com>
2021-07-19 21:58:59 +0530

    edit

--- src/reusables/Nav.js
+++ src/reusables/Nav.js
@@ -42,7 +42,7 @@ const Nav = () => {
                             ></i>
                         </div>
                     </Link>
-                    <div className="col-4">Meeting Maneger</div>
+                    <div className="col-4">Meeting Manager</div>
                     <div className="btn btn-outline-danger"
                         style={{
                             borderRadius: "20%",
